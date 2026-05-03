fc7daefcb263813eaa7437fdd1277239c8584fb5
Daniel Gonzalez <cristopher.gonzalez5445@alumnos.udg.mx>
2026-05-02 18:00:44 -0600

    Agrego ejercicio de tipos de datos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #JAVASCRIPTDE0
+
+
+EL proposito de este proyecno nace principalmente con la intención de desarrollar actividades secuenciales para adquirir los conocimientos de JvaScript desde 0 hasta lograr generar proyectos a nivel profesional.
---
+++ tipos-de-dato.js
@@ -0,0 +1,22 @@
+// Tipos de datos en JavaScript usando typeof
+
+console.log(typeof 42);            
+console.log(typeof 'Veinticinco'); 
+console.log(typeof -666);          
+console.log(typeof true);         
+console.log(typeof 0);             
+console.log(typeof '');            
+console.log(typeof null);          
+console.log(typeof undefined);     
+console.log(typeof "FALSE")
+console.log(typeof false);        
+
+//  Ejemplos adicionales propios
+
+console.log(typeof 3.14);          
+console.log(typeof "Hola mundo");  
+console.log(typeof NaN);           
+console.log(typeof []);           
+console.log(typeof {});            
+console.log(typeof function(){});  
+console.log(typeof Symbol());      
